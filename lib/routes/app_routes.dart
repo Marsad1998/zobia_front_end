634304baa33008b87007a9ca64015521a2d48f2f
zobia <zobia>
2026-06-06 10:55:16 +0500

    first commit

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -10,6 +10,7 @@ class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
+  
   static const String dashboard = '/dashboard';
   static const String adminDashboard = '/admin-dashboard';
 
